ae5734aab9466dd8f59913ad28ef67bcd3a62a8e
NourEldinShobier <nour.eldin.shobier@gmail.com>
2019-05-07 00:03:11 +0200

    Add encoders

--- src/core/Settings.java
+++ src/core/Settings.java
@@ -1,7 +1,7 @@
 package core;
 
 public class Settings {
-    public static boolean isFixedFormat = false;
+    public static boolean isFixedFormat = true;
     public static String filePath = "C:\\SICAssembler\\srcFile.txt";
 
 
---
+++ src/utils/InstructionsEncoders/FormatFOUREnCoder.java
@@ -0,0 +1,9 @@
+package utils.InstructionsEncoders;
+
+import utils.Instruction.Instruction;
+
+public class FormatFOUREnCoder {
+    public static Instruction encode(Instruction instruction){
+        return null;
+    }
+}
