d42319d0f6baf9170e3c09140fa8c537d232d47e
Nour El-Din Shobier <47837759+NourEldinShobier@users.noreply.github.com>
2019-05-12 16:52:55 +0200

    Update RegistersClass

--- src/core/Registers.java
+++ src/core/Registers.java
@@ -1,14 +1,24 @@
 package core;
 
+import java.util.Map;
+import static java.util.Map.entry;
+
 public class Registers
 {
-    String A = "0000";
-    String B = "0010";
-    String S;
-    String T;
-    String F;
-    String X;
-    String L;
-    String PC;
-    String SW;
+    private static Map<String, String> registers = Map.ofEntries(
+            entry("A", "0000"),
+            entry("X", "0001"),
+            entry("L", "0010"),
+            entry("PC", "1000"),
+            entry("SW", "1001"),
+
+            entry("B", "0011"),
+            entry("S", "0100"),
+            entry("T", "0101"),
+            entry("F", "0110")
+    );
+
+    public static String get(String registerName){
+        return registers.get(registerName);
+    }
 }
