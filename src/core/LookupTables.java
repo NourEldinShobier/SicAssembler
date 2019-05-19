8377ab6283da65c05a7cbd71588d328ebf10cfb6
Nour Youssry <eng-nour.yousry1621@alexu.edu.eg>
2019-05-19 16:10:35 +0200

    changed litTab to list of Literals

--- src/core/LookupTables.java
+++ src/core/LookupTables.java
@@ -1,11 +1,12 @@
 package core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class LookupTables
 {
     public static HashMap<String, String> symbolTable = new HashMap<>();
-    public static HashMap<String, String> literalTable = new HashMap<>();
+    public static ArrayList<Literal> literalTable = new ArrayList<Literal>();
 
 
 }
